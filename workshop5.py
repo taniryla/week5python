d229ca5a1f54b81bc99041fba87c522c2351f427
Chako <ryland0000@gmail.com>
2023-08-13 20:28:37 -0700

    workshop5 task 2

--- workshop5.py
+++ workshop5.py
@@ -18,12 +18,29 @@ def guess_random_number(tries: int, start: int, stop: int):
             tries -= 1
         else:
             print(f"You have failed to guess the number: {target}")
+            break
     
 
-guess_random_number(5, 0, 10)
+# guess_random_number(5, 0, 10)
 
 # TASK 2: The computer will generate a random number, then the computer will try to guess it using linear search.
 
+def guess_random_num_linear(tries: int, start: int, stop: int):
+    target = random.randint(start, stop)
+    print(f"The number for the program to guess is: {target}")
+    for num in range(start, stop):
+        if tries > 0 and num != target:
+            print(f"The program is guessing... {num}")
+            print(f"Number of tries left: {tries}")
+            tries -= 1
+        elif tries > 0 and num == target:
+            print(f"The program is guessing... {num}")
+            print("The program has guessed the correct number")
+            return
+        
+    print(f"The program has failed to guess the correct number.")   
+    
 
 
+guess_random_num_linear(5, 0, 10)
 # TASK 3: The computer will generate a random number, then the computer will try to guess it using binary search.
