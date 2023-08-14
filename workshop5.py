9518d64a03f42c91e2c33f883b0d3684221629e5
Chako <ryland0000@gmail.com>
2023-08-13 18:08:09 -0700

    workshop5

---
+++ workshop5.py
@@ -0,0 +1,8 @@
+# The computer will generate a random number, then you will try to guess it.
+
+
+# The computer will generate a random number, then the computer will try to guess it using linear search.
+
+
+
+# The computer will generate a random number, then the computer will try to guess it using binary search.
