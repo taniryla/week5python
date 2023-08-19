b06010251a74bf1fd66411decb48c94fdbbac367
Chako <ryland0000@gmail.com>
2023-08-18 19:32:12 -0700

    war_game made code more DRY

--- war_game.py
+++ war_game.py
@@ -18,6 +18,10 @@ computer_cards = []
 # 2.4) Each player turns one card face up and one card face down
 warPlayerDeck = []
 warComputerDeck = []
+# 2.5) Create a variable of faceUpPlayerCard 
+faceUpPlayerCard = None
+# 2.6) Create a variable of faceUpComputerCard
+faceUpComputerCard = None
 
 # /*----- cached element references -----*/
 # 3) Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant:
@@ -32,86 +36,77 @@ warComputerDeck = []
 # /*----- functions -----*/
 
 # 4) Upon loading the app should:
-
+def init():
 #	4.1) Initialize the state variables:
-#   4.1.1) Create a variable of faceUpPlayerCard 
-faceUpPlayerCard = None
-#   4.1.2) Create a variable of faceUpComputerCard
-faceUpComputerCard = None
+#  
 #   4.1.3) Create a variable for winner
-winner = None
+    winner = None
+    build()
+    shuffle_deck()
+    deal_deck()
+    draw(0)
 
 #	4.2) Build a list for deck 
-
 deck = []
-
 
 def build():
     for s in suits:
         for r in ranks:
             deck.append((s, r))
 
-build()
-    
-#       4.3) Shuffle the deck
+#   4.3) Shuffle the deck
+def shuffle_deck():
+    random.shuffle(deck)
 
-random.shuffle(deck)
-
-        # 4.4) Player and Computer turn up one card at the same time from the top of their card stack and the player with the higher card takes both cards and puts on the bottom of their card stack
-def deal_deck():
-#  Player_cards receives 26 random cards out of a 52 card deck. computer_cards receives 26 random cards. 
-    # splice 1 to 26 of deck list into player_cards
-    player_cards.append(deck[0:int(len(deck)/2)])
-    # print(f"Player cards... {player_cards}")
-    # splice remaining 27 to 52 to the list computer_cards
-    computer_cards.append(deck[(int(len(deck)/2)+1):52])
-    # print(f"Computer cards... {computer_cards}")
-
-
-deal_deck()
+#   4.4) Player and Computer turn up one card at the same time from the top of their card stack and the player with the higher card takes both cards and puts on the bottom of their card stack
+def deal_deck(): #  Player_cards receives 26 random cards out of a 52 card deck. computer_cards receives 26 random cards. 
+    player_cards.append(deck[0:int(len(deck)/2)]) # splice 1 to 26 of deck list into player_cards
+    computer_cards.append(deck[(int(len(deck)/2)+1):52]) # splice remaining 27 to 52 to the list computer_cards
 
 #   4.5) Each player draws a card
-
-faceUpPlayerCard = player_cards[0].pop() # the top item is removed from the player_cards list
-player_face_value = faceUpPlayerCard[1]
-# print(f"Faceup Player Card Value... {player_face_value}")
-faceUpComputerCard = computer_cards[0].pop()
-computer_face_value = faceUpComputerCard[1]
-# print(f"Faceup Computer Card Value... {computer_face_value}")
-
-print(f"testing... {player_cards[0]}")
+def draw(x):
+    faceUpPlayerCard = player_cards[x].pop() # the top item is removed from the player_cards list
+    player_face_value = faceUpPlayerCard[1]
+    faceUpComputerCard = computer_cards[x].pop()
+    computer_face_value = faceUpComputerCard[1]
+    determine_winner(player_face_value, computer_face_value, faceUpPlayerCard, faceUpComputerCard)
 
 #   4.6) If Player and Computer turn up the same card, then invoke a go_to_war() function
 def go_to_war():        
     # if tie, three cards face down and then card face-up, winner takes all 10 cards
-    warPlayerDeck.append(player_cards[0:4])
-    print(f"War Cards... {warPlayerDeck}")
+    warPlayerDeck.append(player_cards[0])
+    warPlayerDeck.append(player_cards[1])
+    warPlayerDeck.append(player_cards[2])
+    warComputerDeck.append(computer_cards[0])
+    warComputerDeck.append(computer_cards[1])
+    warComputerDeck.append(computer_cards[2])
+    draw(3)   # If player and computer tie, repeat the go_to_war() function with one card face up and one card face down.
+    print(f"Hidden Cards... {warPlayerDeck} {warComputerDeck}")
 
-    # If player and computer, repeat the go_to_war() function with one card face up and one card face down.
-
-    # if player or computer doesn't have 3 cards to put face-down, then they lose the war
+    # TODO1: if player or computer doesn't have 3 cards to put face-down, then they lose the war
 
 #   4.7) Compare Player and Computer cards up to see who wins
+def determine_winner(j, k, l, m):
+    if j > k: # both face up cards by player and computer go to player_cards list
+        player_cards.insert(0, l)
+        player_cards.insert(1, m)
+#   OPTIONAL TODO2: Add J, Q, K and A back into the 11, 12, 13 and 14 values
+        # TODO3: Show number of cards for player and include in print below. Check if ekther player_cards or computer_cards is empty and if either are empty determine winner
+        print(f"Player shows the {l[1]} of {m[0]} and computer shows the {m[1]} of {l[0]}. (Ace is 14, King is 13, Queen is 12, and Jack is 11). Player wins!")
+        l = None
+        m = None
+        draw(0)
+    elif j > k: # both face up cards by player and computer go to computer_cards list
+        computer_cards.insert(0, l)
+        computer_cards.insert(1, m)
+        # TODO4: Show number of cards for computer and include in print below. Check if ekther player_cards or computer_cards is empty and if either are empty determine winner
+        print(f"Player shows the {l[1]} of {m[0]} and computer shows the {m[1]} of {l[0]}. (Ace is 14, King is 13, Queen is 12, and Jack is 11). Computer wins!")
+        l = None
+        m = None
+        draw(0)
+    elif j == k:
+        go_to_war()
 
-if player_face_value > computer_face_value: # both face up cards by player and computer go to player_cards list
-    player_cards.insert(0,faceUpPlayerCard)
-    player_cards.insert(1, faceUpComputerCard)
-    print(f"Player shows the {faceUpPlayerCard[1]} of {faceUpPlayerCard[0]} and computer shows the {faceUpComputerCard[1]} of {faceUpComputerCard[0]}. (Ace is 14, King is 13, Queen is 12, and Jack is 11). Player wins!")
-    faceUpPlayerCard = None
-    faceUpComputerCard = None
-elif computer_face_value > player_face_value: # both face up cards by player and computer go to computer_cards list
-    computer_cards.insert(0, faceUpPlayerCard)
-    computer_cards.insert(1, faceUpComputerCard)
-    print(f"Player shows the {faceUpPlayerCard[1]} of {faceUpPlayerCard[0]} and computer shows the {faceUpComputerCard[1]} of {faceUpComputerCard[0]}. (Ace is 14, King is 13, Queen is 12, and Jack is 11). Computer wins!")
-    faceUpPlayerCard = None
-    faceUpComputerCard = None
-else:
-    go_to_war()
 
 
-
-
-#   4.8 OPTIONAL) Add J, Q, K and A back into the 11, 12, 13 and 14 values
-# 
-#   4.9) Check if ekther player_cards or computer_cards is empty and if either are empty determine winner
-
+init()
